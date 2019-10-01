fc3a8b7ca97b97f095086e5c30ce77b2e85b568b
Young Min <dudals3844@gmail.com>
2019-10-01 04:22:48 +0000

    learn Dictionary

--- src/test.py
+++ src/test.py
@@ -1,3 +1,26 @@
-#tuple tuple is almost same list but can't change value
-t1  = (1,2,3)
-#del t1[0] tuple can't remove index
+#Dictionary
+dic = {'name':'pey','phone':'01099533844','birth':'970912'}
+
+#plus dictionary
+dic['sex'] = 'male'
+print(dic)
+
+#remove dictionary
+del dic['birth']
+print(dic)
+
+#key and value
+print(dic['phone'])
+
+#print key
+print(dic.keys())
+
+#print value
+print(dic.values())
+
+#print key and value both
+print(dic.items())
+
+#cofirm key in dictionary
+print('male' in dic)
+print('name' in dic)
