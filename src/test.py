f97614f1858285c9db8857989876062bfd5453b7
Young Min <dudals3844@gmail.com>
2019-10-01 04:06:05 +0000

    learn list

--- src/test.py
+++ src/test.py
@@ -1,45 +1,60 @@
-word = "Hello"
+#laern list
 
-print(word)
+odd = [1,2,3,4,5]
 
-wordlinechange = "Hello \n World"
+print(odd[0])
 
-print(wordlinechange)
+print(odd[-1])
 
-head = "Python"
-body = " is fun"
+a = [1,2,3,['a','b','c']]
+print(a[0])
+print(a[-1])
+print(a[-1][0])
 
-print(head+body)
+b = [1,2,3,4,5]
 
+print(b[0:2])
 
-a = "Python"
-print(a * 2)
+a = [1,2,3]
+b = [4,5,6]
+print(a+b)
 
-#string length
-b = "Life is too short"
-print(len(b))
+#repeat list
+print(a*3)
+#list length
+print(len(a))
 
-num = 3
-banana = 4
-print("I want eat %d apples and %d banana"%(num,banana))
-# using format
-print("I want eat {0} apples and {1} banana".format(num,banana))
-print("I want eat {0} apples and {banana_index} banana".format(num,banana_index=banana))
+#error a + "hi" because different type
+print(str(a)+"hi")
 
-#f fomating
-name = "Choi"
-f'MY name is {name}'
+#edit list
+a = [1,2,3]
+a[1] = 100
+print(a)
 
-#char count
-a = "hobby"
-print(a.count('b')) #2 'b' in hobby
+#delete list element
+del a[1]
+print(a)
 
-#find location char
-print(a.find('b'))
+#plus list element
+a = [1,2,3]
+a.append(4)
+print(a)
 
-#splite string
-a = "Latte is horse"
-print(a.split())
+a = [4,3,2,1]
+a.sort()
+print(a)
 
-b = "a:b:c:d"
-print(b.split(':'))
+a = [1,2,3,4]
+a.reverse()
+print(a)
+
+print(a.index(1))
+
+a.insert(0, 77)
+print(a)
+
+print(a.count(77))
+
+a.extend([99,100])
+print(a)
