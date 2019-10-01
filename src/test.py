1ac9cad4a19e1abbd587eb71e83829f3fcea89f8
Young Min <dudals3844@gmail.com>
2019-10-01 02:08:10 +0000

    learn string type

--- src/test.py
+++ src/test.py
@@ -1,14 +1,45 @@
-a = 3
-b = 4
+word = "Hello"
 
-print(a ** b)
+print(word)
 
-print(a/b) #0.75
+wordlinechange = "Hello \n World"
 
-print(a//b) #0
+print(wordlinechange)
 
-#8
-a = 0o177
+head = "Python"
+body = " is fun"
 
-#16
-a = 0x8ff
+print(head+body)
+
+
+a = "Python"
+print(a * 2)
+
+#string length
+b = "Life is too short"
+print(len(b))
+
+num = 3
+banana = 4
+print("I want eat %d apples and %d banana"%(num,banana))
+# using format
+print("I want eat {0} apples and {1} banana".format(num,banana))
+print("I want eat {0} apples and {banana_index} banana".format(num,banana_index=banana))
+
+#f fomating
+name = "Choi"
+f'MY name is {name}'
+
+#char count
+a = "hobby"
+print(a.count('b')) #2 'b' in hobby
+
+#find location char
+print(a.find('b'))
+
+#splite string
+a = "Latte is horse"
+print(a.split())
+
+b = "a:b:c:d"
+print(b.split(':'))
